ffd0fc91dcd3c190a68919e8bd1d3aafb97458f3
유건우 <dbrjsdn1220@gmail.com>
2025-03-13 14:17:02 +0900

    중고차 가격 예측 프로젝트

---
+++ main.py
@@ -0,0 +1,89 @@
+import math
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import KFold
+from sklearn.metrics import mean_squared_error
+from sklearn.linear_model import LinearRegression
+
+# 분석할 데이터
+# --------------------------------------------------------
+df_car = pd.read_csv("cars.csv", encoding='cp949')
+
+# 가격에 연식이 미치는 영향이 크므로 차이를 주기
+df_car['연식'] = (df_car['연식'] - 1985) ** 5
+
+# 차량 기본 가격 추가
+price = [5890, 6478, 7907, 3645, 2489, 2842, 3877, 3024, 3090]
+new_price = []
+for car in np.array(df_car):
+    if car[0] == 0:
+        new_price.append(price[0])
+    elif car[0] == 1.0:
+        new_price.append(price[1])
+    elif car[0] == 2.0:
+        new_price.append(price[2])
+    elif car[0] == 3.0:
+        new_price.append(price[3])
+    elif car[0] == 4.0:
+        new_price.append(price[4])
+    elif car[0] == 5.0:
+        new_price.append(price[5])
+    elif car[0] == 6.0:
+        new_price.append(price[6])
+    elif car[0] == 7.0:
+        new_price.append(price[7])
+    elif car[0] == 8.0:
+        new_price.append(price[8])
+
+df_car['기본가격'] = new_price
+
+print(df_car.columns)
+print(df_car)
+# --------------------------------------------------------
+
+# Linear Regression 모델 만들기
+# --------------------------------------------------------
+X = np.array(df_car.drop(['가격(단위: 만)'], axis=1, inplace=False))
+Y = np.array(df_car['가격(단위: 만)'])
+
+# 데이터 양이 적으므로 k겹 교차검증
+kf = KFold(n_splits=5, shuffle=True, random_state=0)
+list_rmse = []
+all_Y_pred = []
+all_Y_test = []
+
+for train_index, test_index in kf.split(X):
+    X_train, X_test = X[train_index], X[test_index]
+    Y_train, Y_test = Y[train_index], Y[test_index]
+
+    # 모델 학습
+    model = LinearRegression()
+    model.fit(X_train, Y_train)
+
+    Y_pred = model.predict(X_test)
+    # 가끔 음수 값이 나와서 절대값 처리
+    Y_pred = np.abs(Y_pred)
+
+    all_Y_pred.extend(Y_pred)
+    all_Y_test.extend(Y_test)
+
+    mse = mean_squared_error(Y_test, Y_pred)
+    list_rmse += [math.sqrt(mse)]
+
+# K겹 교차검증 결과 출력
+print('K겹 교차검증(옵션/연식 수정, 기본가 추가)')
+print('5개 평균: %f\n' % np.mean(list_rmse))
+for i in range(len(list_rmse)):
+    print('[%d]RMSE: %f' % (i, list_rmse[i]))
+# --------------------------------------------------------
+
+# 실제값과 예측값 산포도 그래프
+# --------------------------------------------------------
+plt.scatter(all_Y_test, all_Y_pred, color='blue', alpha=0.6)
+plt.plot([min(all_Y_test), max(all_Y_test)], [min(all_Y_test), max(all_Y_test)], color='black', linestyle='--')
+plt.xlabel('Actual Price')
+plt.ylabel('Predicted Price')
+plt.title('Actual vs Predicted Price (FINAL)')
+plt.show()
+# --------------------------------------------------------
---
+++ web_crawling.py
@@ -0,0 +1,126 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+# 차량 데이터 프레임 생성
+# -----------------------------------------------
+car_columns = ['차종', '가격(단위: 만)', '보험처리', '연식', '배기량', '주행거리', '연비']
+url = "https://bobaedream.co.kr/mycar/popup/option_explain.php"
+response = requests.get(url)
+soup = BeautifulSoup(response.text, "html.parser")
+data = soup.find_all("ul", class_="dic-list")
+
+for i in range(len(data)-1):
+    for j in data[i].find_all("li"):
+        car_columns += [j.get_text()]
+
+df_car = pd.DataFrame(columns=car_columns)
+df_car = df_car.astype(float)
+# -----------------------------------------------
+
+# 분석할 차량 상세 페이지 url 크롤링
+# -----------------------------------------------
+# 순서대로 [G80, GV80, G90, 그랜저, 쏘나타, 싼타페, 카니발, 쏘렌토, K7]
+car_urls = ["https://www.bobaedream.co.kr/mycar/mycar_list.php?gubun=K&maker_no=1010&group_no=893&page=%d",
+            "https://www.bobaedream.co.kr/mycar/mycar_list.php?gubun=K&maker_no=1010&group_no=1032&page=%d",
+            "https://www.bobaedream.co.kr/mycar/mycar_list.php?gubun=K&maker_no=1010&group_no=958&page=%d",
+            "https://www.bobaedream.co.kr/mycar/mycar_list.php?gubun=K&maker_no=3&group_no=76&page=%d",
+            "https://www.bobaedream.co.kr/mycar/mycar_list.php?gubun=K&maker_no=3&group_no=69&page=%d",
+            "https://www.bobaedream.co.kr/mycar/mycar_list.php?gubun=K&maker_no=3&group_no=45&page=%d",
+            "https://www.bobaedream.co.kr/mycar/mycar_list.php?gubun=K&maker_no=3&group_no=76&page=%d",
+            "https://www.bobaedream.co.kr/mycar/mycar_list.php?gubun=K&maker_no=3&group_no=69&page=%d",
+            "https://www.bobaedream.co.kr/mycar/mycar_list.php?gubun=K&maker_no=3&group_no=45&page=%d"]
+cars = []
+
+for car_type in range(len(car_urls)):
+    detail_urls = []
+    for i in range(1, 20):
+        car_url = (car_urls[car_type] % i)
+        response = requests.get(car_url)
+        soup = BeautifulSoup(response.text, "html.parser")
+        data = soup.find_all("p", class_="tit")
+        # 예외 처리 (차량 정보가 더 이상 없는 경우)
+        try:
+            temp = data[0]
+        except IndexError:
+            break
+
+        # 차량 상세 페이지 url 수집
+        for j in range(len(data)):
+            href = data[j].a['href']
+            detail_urls += ["https://www.bobaedream.co.kr" + href]
+    # -----------------------------------------------
+
+    # 차량 상세 페이지로 들어가 상세 정보 크롤링
+    # -----------------------------------------------
+    for detail_url in detail_urls:
+        print("작동 ")
+        response = requests.get(detail_url)
+        soup = BeautifulSoup(response.text, "html.parser")
+        options = soup.find_all("span", class_="radioBox")
+        # 예외 처리 (옵션 정보가 없는 차량은 Feature를 만들 수 없어 스킵)
+        try:
+            check = soup.find_all("div", class_="option-list-container")[0]
+        except IndexError:
+            print(".", end="")
+            continue
+
+        # 차종
+        car = [car_type]
+
+        # 가격(단위: 만) / 예외(렌트/리스, 계약 차량 제외)
+        data = soup.find_all("b", class_="cr")
+        try:
+            car += [float(data[0].get_text().replace(",", ""))]
+            if soup.find_all("h4", class_="tit")[1].get_text() == "렌트정보":
+                print(".", end="")
+                continue
+            elif soup.find_all("h4", class_="tit")[1].get_text() == "리스정보":
+                print(".", end="")
+                continue
+            elif soup.find("span", class_="price").get_text() == "[계약]":
+                print(".", end="")
+                continue
+        except IndexError:
+            print(".", end="")
+            continue
+
+        # 보험 처리 / 예외(보험 이력 없는 차 -> 판매자 주장은 무사고)
+        try:
+            car += [int(data[1].get_text())]
+        except IndexError:
+            car += [0]
+
+        # 연식, 배기량, 주행 거리
+        data = soup.find("div", class_="tbl-01 st-low").table.tbody.find_all("td")
+        car += [int(data[0].get_text().split(".")[0])]
+        car += [int(data[1].get_text().split(" ")[0].replace(",", ""))]
+        car += [int(data[2].get_text().split(" ")[0].replace(",", ""))]
+
+        # 연비 / 예외(연비 표시 안한 차량 -> 새차 기준 최저 평균 8.5km/l로 추가)
+        try:
+            car += [float(soup.find_all("strong", class_="txt")[1].get_text().split("k")[0])]
+        except ValueError:
+            car += [8.5]
+
+        # 차량 옵션 데이터 수집
+        data = soup.find_all("div", class_="tbl-01 st-low")
+        for j in range(len(options)):
+            # 예외 처리(없는 옵션 확인)
+            try:
+                temp = options[j].input['checked']
+                car += [1]
+            except KeyError:
+                car += [0]
+        cars.append(car)
+        print(".", end="")
+    # -----------------------------------------------
+
+# cars 출력 및 csv 파일로 저장
+for i in range(len(cars)):
+    print(i, ": ", cars[i])
+    df_car.loc[len(df_car)] = cars[i]
+
+# csv 파일로 저장
+print("끝!")
+df_car.to_csv('cars.csv', index=False, encoding='cp949')
